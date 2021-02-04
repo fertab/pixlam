1a804a54d8782981b43355c50442df15d29d5175
Fernando Taboada <fjtaboada@teco.com.ar>
2021-02-04 01:08:53 -0300

    1.6

--- contact.php
+++ contact.php
@@ -1,14 +1,19 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
 
     $to = 'info@pixlam.com'; // this is your Email address
     $email = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+    $aditional_headers = array ("From:" . $email , "Reply-To:" . $email . "X-Mailer: PHP/" . PHP_VERSION);
 
-    $headers = "From:" . $email;
-    'X-Mailer: PHP/' . phpversion();
-    mail($to,$message,$headers);
+
+    if ($_POST['message']) {
+    mail('info@pixlam.com',$message,[$aditional_headers]);
     header('Location: greet.html');
-    
+    }
+
 ?>
